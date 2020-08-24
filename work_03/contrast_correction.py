b5ce86065538a7b54b82a463971d24587f7bb63d
DiegoOno <diego.ono1505@gmail.com>
2020-08-24 10:35:57 -0300

    Histogram equalization implemented

--- work_03/contrast_correction.py
+++ work_03/contrast_correction.py
@@ -4,22 +4,16 @@ import argparse
 import sys
 
 def histogram_equalization(image):
- 
-  hist,bins = np.histogram(image.ravel(),256,[0,255])
-
-  # comultative_frequency = np.cumsum(hist)
-  # total_pixels = image.shape[0] * image.shape[1]
-  
-  # LUT = np.uint8((comultative_frequency / total_pixels))
-
-  # equalized = LUT[np.uint8(image[::])] 
-  # return equalized
+  hist,bins = np.histogram(image.flatten(), 256, [0,255])
+  cdf = hist.cumsum()
+  cdf = 255 * cdf / cdf[-1]
+  equalized_image = np.interp(image, range(0, 256), cdf)
+  return equalized_image.astype(np.uint8)
 
 def join_equalized_with_otsu(equalized, otsu_image):
   otsu_image = np.clip(np.floor(otsu_image - 254), 0, 255)
   final_image = equalized * otsu_image
-  final_image = np.uint8(final_image)
-  return final_image
+  return final_image.astype(np.uint8)
 
 def main():
   np.set_printoptions(threshold=sys.maxsize)
@@ -34,6 +28,7 @@ def main():
 
   # imgf is original image with Otsu
   ret, imgf = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+
   equalized = histogram_equalization(img)
   result = join_equalized_with_otsu(equalized, imgf)
 
