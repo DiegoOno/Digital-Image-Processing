1a77a44ba766446d63f89b76d156406d0a3aca68
DiegoOno <diego.ono1505@gmail.com>
2020-08-23 22:13:48 -0300

    Implementing High boost according to Gonzalez's book

--- work_03/contrast_correction.py
+++ work_03/contrast_correction.py
@@ -3,22 +3,23 @@ import cv2
 import argparse
 import sys
 
-def gamma_correction(image, value):
-  LUT = np.arange(256)
-  LUT = np.power(LUT, value)
-  amax = LUT.max()
-  LUT = 256 * LUT / amax
-  LUT = np.uint8(LUT)
-  out = np.zeros(image.shape, dtype=np.uint8)
-  out = LUT[image]
-  return out
+def histogram_equalization(image):
+ 
+  hist,bins = np.histogram(image.ravel(),256,[0,255])
 
-def join_gamma_corrected_with_otsu(gamma_corrected_image, otsu_image):
+  # comultative_frequency = np.cumsum(hist)
+  # total_pixels = image.shape[0] * image.shape[1]
+  
+  # LUT = np.uint8((comultative_frequency / total_pixels))
+
+  # equalized = LUT[np.uint8(image[::])] 
+  # return equalized
+
+def join_equalized_with_otsu(equalized, otsu_image):
   otsu_image = np.clip(np.floor(otsu_image - 254), 0, 255)
-  final_image = gamma_corrected_image * otsu_image
+  final_image = equalized * otsu_image
   final_image = np.uint8(final_image)
   return final_image
-
 
 def main():
   np.set_printoptions(threshold=sys.maxsize)
@@ -30,15 +31,16 @@ def main():
   args = vars(ap.parse_args())
   
   img = cv2.imread(args['image'], 0)
-  
+
+  # imgf is original image with Otsu
   ret, imgf = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
-  gamma_corrected_image = gamma_correction(img, 0.5)
-  final_image = join_gamma_corrected_with_otsu(gamma_corrected_image, imgf)
+  equalized = histogram_equalization(img)
+  result = join_equalized_with_otsu(equalized, imgf)
 
   cv2.imshow('Original', img)
   cv2.imshow('Otsu', imgf)
-  cv2.imshow('Gamma correction', gamma_corrected_image)
-  cv2.imshow('Gamma + Otsu', final_image)
+  cv2.imshow('Equalized', equalized)
+  cv2.imshow('Otsu + Equalization', result)
 
   cv2.waitKey(0)
   cv2.destroyAllWindows()
---
+++ work_04/high_boost.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+import argparse
+import sys
+
+def gaussian_filter(shape=(3,3),sigma=0.5):
+  m,n = [(ss-1.)/2. for ss in shape]
+  y,x = np.ogrid[-m:m+1,-n:n+1]
+  h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+  h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+  sumh = h.sum()
+  if sumh != 0:
+    h /= sumh
+  return h
+
+def main():
+  np.set_printoptions(threshold=sys.maxsize)
+  ap = argparse.ArgumentParser()
+
+  ap.add_argument('-i', '--image', required=True, help='Caminho da imagem')  
+  ap.add_argument('-w', '--weight', required=True, help='Peso de generalização') # Need change   
+  args = vars(ap.parse_args())
+
+  image_path = args['image']
+  weight = float(args['weight'])
+  # image_name = image_path.split('.')[0]
+  img = cv2.imread(image_path, 0)
+
+  # Convolution with gaussian mask
+  gauss_mask = gaussian_filter()
+  gauss_convolved = cv2.filter2D(img, -1, gauss_mask)
+
+  high_boost_mask = img - gauss_convolved
+  print(high_boost_mask)
+
+  # g(x, y) = f(x, y) + k * Gmask(x, y)
+  high_boost_image = np.add(img, high_boost_mask)
+  high_boost_image = np.uint8(high_boost_image)
+
+  # cv2.imwrite(image_name + '_m2.jpg', new_image)
+  cv2.imshow('Original Image', img)
+  cv2.imshow('Gaussian mask', gauss_convolved)
+  cv2.imshow('High Boost Mask', high_boost_mask)
+  cv2.imshow('High boost image', high_boost_image)
+
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+	
+if (__name__ == '__main__'):
+  main()
